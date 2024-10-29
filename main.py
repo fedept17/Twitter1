4e36251f47647060535fc46e8b3610c96c9a69d2
sinuhajii02 <cleojoe10@gmail.com>
2024-10-29 20:40:46 +0100

    update
    array with prio

--- main.py
+++ main.py
@@ -4,13 +4,18 @@ from neca.events import *
 from neca.settings import app, socket
 from flask import Flask, request
 import smtplib
-
 import socketio
 
+
+## default values
 send_email_user = {}
 selected_location = ""
 selected_priorities = {}
 tweets = []
+gmail = "incidenthubweek9@gmail.com"
+password = "mikv iudj fwqh dobp" # app password
+# real = 'utwenteproject2024!'
+
 
 #pie chart
 def add_data_pie_chart(data):
@@ -24,21 +29,6 @@ def check_location_email(data):
     if data['region'].lower() in send_email_user:
         for email in send_email_user[data['region'].lower()]:
             send_email(email, data)
-        
-
-@event("tweet")
-def tweet_event(context, data):
-    tweets.append(data)
-    emit("nationwide", data)
-    add_data_pie_chart(data)
-    check_location_email(data)
-    print(data['region'])
-    
-generate_data('p2000_incidents.json',
-              time_scale=50,
-              event_name='tweet',
-              limit=1000)
-
 def send_email(receiver_email, data):
     try:
         with smtplib.SMTP("smtp.gmail.com", port=587) as connection:
@@ -62,12 +52,41 @@ def send_email(receiver_email, data):
         print(f"An error occurred: {e}")
 
 
-gmail = "incidenthubweek9@gmail.com"
-password = "mikv iudj fwqh dobp" # app password
-# real = 'utwenteproject2024!'
+@app.route("/api/form", methods=["POST"])
+def form():
+    # Extract JSON data from the request
+    data = request.json
 
-@app.route("/filter", methods=["POST"])
-def filter(data):
+    email = data['email'].lower()
+    location = data['location'].lower()
+    if location not in send_email_user:
+        send_email_user[location] = []
+    send_email_user[f'{location}'].append(email)
+
+    return "ok", 200
+
+# add nationwide
+def add_nationwide_data(data):
+    emit("nationwide", data)
+
+
+# append tweets with priority
+def get_alert_priority(data):
+    if data['description'][:1] in ['1', '2', '3']:
+        tweets.append([data, data['description'][:1]])
+    elif data['description'][:2] in ['A1', 'A2', 'A3']:
+        tweets.append([data, data['description'][:2].replace("A", "")])
+    elif data['description'][:3] in ['P 1', 'P 2', 'P 3']:
+        tweets.append([data, data['description'][:3].replace("P ", "")])
+    elif data['description'][:6] in ['Prio 1', 'Prio 2', 'Prio 3']:
+        tweets.append([data, data['description'][:6].replace("Prio ", "")])
+    else:
+        tweets.append([data, "non-priority"])
+
+
+# filter priority
+@app.route("/filter_priority", methods=["POST"])
+def filter_priority(data):
     filter_priorities = request.json
     print('received json: ' + str(filter_priorities))
 
@@ -76,24 +95,19 @@ def filter(data):
     
     socketio.emit("location", {"data": filtered_data})
 
-@app.route("/api/form", methods=["POST"])
-def form():
-    # Extract JSON data from the request
-    data = request.json
 
-    # Convert email and location to lowercase
-    email = data['email'].lower()
-    location = data['location'].lower()
-    if location not in send_email_user:
-        send_email_user[location] = []
-    send_email_user[f'{location}'].append(email)
-    # Prepare the data to be sent to the event
-    lowercased_data = {'email': email, 'location': location}
-    print(send_email_user)
-    # Fire the global event with the modified data
 
-    return "ok", 200
+@event("tweet")
+def tweet_event(context, data):
+    add_nationwide_data(data)
+    add_data_pie_chart(data)
+    check_location_email(data)
+    get_alert_priority(data)
 
+generate_data('p2000_incidents.json',
+              time_scale=10,
+              event_name='tweet',
+              limit=1000)
 
 # starts the server and prevents the program from exiting
-neca.start()
+neca.start(port=3100)
